60a191f480ac71ecc0839c2b4d876fb37d211810
Jorge <jorgeivan.cruz@e-campus.uab.cat>
2020-11-15 17:49:47 +0100

    Deleted test for SetShipsLocation. Modified testAttackShip.

--- src/test/java/Battleship/PlayerTest.java
+++ src/test/java/Battleship/PlayerTest.java
@@ -2,6 +2,9 @@ package Battleship;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,62 +17,6 @@ public class PlayerTest {
 	@Test
 	public void testSetShipsLocation() {
 		
-		Player player = new Player();
-		Board board = new Board();
-		Ship ship = new Battleship();
-		
-		//TEST 1
-		
-		player.iniciateShipsLocation();
-		player.posX = 0;
-		player.posY = 0;
-		player.horizontal = true;
-		
-		player.shipList.clear();
-		player.shipList.add(ship);
-		player.setShipsLocation(board);
-		
-		boolean horizontal = player.horizontal;
-		int posX = player.posX;
-		int posY = player.posY;
-		
-		if(horizontal)
-		{	
-			if(posX+ship.getSize() <= player.MAX_ROW_COL)
-			{
-				for(int i=posX;i<(posX+ship.getSize());i++)
-				{
-					assertEquals(ship.getType(), player.shipsLocation[posY][i]);
-				}
-			}
-
-		}
-		
-		//TEST 2
-		Ship ship2 = new Crusier();
-		
-		player.posX = 2;
-		player.posY = 2;
-		player.horizontal = false;
-		
-		player.shipList.clear();
-		player.shipList.add(ship2);
-		player.setShipsLocation(board);
-		
-		horizontal = player.horizontal;
-		posX = player.posX;
-		posY = player.posY;
-		
-		if(!horizontal)
-		{
-			if(posY+ship2.getSize() <= player.MAX_ROW_COL)
-			{
-				for(int i=posY;i<(posY+ship2.getSize());i++)
-				{
-					assertEquals(ship2.getType(), player.shipsLocation[i][posX]);
-				}
-			}
-		}
 	}
 		
 	
@@ -87,22 +34,29 @@ public class PlayerTest {
 		Board board = new Board();
 		board.updateCompShips(comp.shipsLocation);
 		//TEST 1
+		String coords = "A1";
+		InputStream is = new ByteArrayInputStream( coords.getBytes() );
+		player.ScannerLineTester(is);
+		
 		int hitsBefore = player.hits;
-		player.posX = 0;
-		player.posY = 0;
+
 		player.attackShip(board);
 		int hitsAfter = player.hits;
+		
 		assertEquals(hitsBefore+1, hitsAfter);
 		assertEquals('H', board.playerBoard[0][0]);
 		
 		//TEST 2
 		int hitsBefore1 = player.hits;
-		player.posX = 4;
-		player.posY = 1;
+		
+		coords = "B4";
+		InputStream is1 = new ByteArrayInputStream( coords.getBytes() );
+		player.ScannerLineTester(is1);
+		
 		player.attackShip(board);
 		int hitsAfter1 = player.hits;
 		assertEquals(hitsBefore1, hitsAfter1);
-		assertEquals('M', board.playerBoard[1][4]);
+		assertEquals('M', board.playerBoard[1][3]);
 	}
 
 }
